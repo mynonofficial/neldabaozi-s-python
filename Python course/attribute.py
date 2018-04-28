73499a484fdecbe8c49de98422bff10ceee3a621
Barish Ahsen <38763575+mynonofficial@users.noreply.github.com>
2018-04-28 15:32:21 +0300

    plus1

--- Python course/attribute.py
+++ Python course/attribute.py
@@ -3,7 +3,8 @@ class Student(object):
 
     def __init__(self, name):
         self.name = name
+        
         if self.name != " ":
-            Student.count = Student.count + 1
+            Student.count += 1
         else:
             Student.count = Student.count
